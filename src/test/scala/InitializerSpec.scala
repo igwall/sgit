464facfdec17583f37b498ed70c54bd0a7823d01
Lucas Gonçalves <lucas.pro@mail.com>
2019-10-02 23:56:24 +0200

    [WIP] Test environment

---
+++ src/test/scala/InitializerSpec.scala
@@ -0,0 +1,14 @@
+package example
+import java.io.File
+
+import org.scalatest._
+import app.Initializer
+
+class InitializerSpec extends FlatSpec with Matchers {
+  "The initializer" should "create a .sgit folder with folders" in {
+    val initializer = new Initializer()
+    initializer.initialise
+    val file = new File(".sgit").mkdir()
+    assert(file)
+  }
+}
